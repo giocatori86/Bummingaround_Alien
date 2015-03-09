d28747683447570596034e7d994efb6fc8d2102c
Matteo De Carlo <matteo.dek@gmail.com>
2015-03-09 17:55:34 +0100

    first commit

---
+++ app/src/main/java/com/example/iwa/pollutiontracking/PositionTrackingService.java
@@ -0,0 +1,18 @@
+package com.example.iwa.pollutiontracking;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class PositionTrackingService extends Service {
+
+    public PositionTrackingService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+}
